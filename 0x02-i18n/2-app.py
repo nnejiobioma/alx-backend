043b61dd1a4402d6743d6287030380109737a5a2
nnejiobioma <nnejiobioma@gmail.com>
2023-10-31 16:51:10 +0000

    task amended

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""This is a simple flask app
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Babel instantiate """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Locale language to determine the
+    best match with support
+    language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world():
+    """welcome greetings
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
